7b5bbc2d5a9f4ad496131e2c616af1ed95979e6a
Omkarbade <omakrbade3799@gmail.com>
2022-02-16 00:08:51 +0530

    [Omkar] Add BinarySearchTree program

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+            BinarySearchTree();
+            Console.ReadLine();
+        }
+        //Method to Initialize Nodes
+        public static void BinarySearchTree()
+        {
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+            tree.CreateNode(56);
+            tree.CreateNode(30);
+            tree.CreateNode(70);
+            Console.WriteLine("Display Nodes present in Binary Search Tree");
+            tree.Display();
+        }
+    }
+    
+}
